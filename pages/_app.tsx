d810ded2a82f09016030453191c9127c1b6af518
Adebayo Temitope <adecancode@gmail.com>
2022-07-23 01:17:19 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import 'styles/index.scss';
 import type { AppProps } from 'next/app';
 
+// inspration - https://dribbble.com/shots/18734842-NFT-Marketplace-Website
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
